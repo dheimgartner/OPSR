496ed7ab824c58ce0236ce6f251638b114d82276
dheimgartner <d.heimgartners@gmail.com>
2024-10-08 21:38:45 +0200

    blobb

--- src/utils.h
+++ src/utils.h
@@ -3,7 +3,7 @@
 
 #include <RcppArmadillo.h>
 
-Rcpp::List opsr_prepare_coefs(Rcpp::NumericVector& theta, int& nReg);
+Rcpp::List opsr_prepare_coefs(Rcpp::NumericVector& theta, int nReg);
 double dnorm_double(double x);
 double pnorm_double(double x);
 
