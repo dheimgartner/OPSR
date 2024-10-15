c8af152207f5fd0b283f324c99342193d4ad1613
dheimgartner <d.heimgartners@gmail.com>
2024-10-15 22:48:56 +0200

    citation and startup message

---
+++ R/zzz.R
@@ -0,0 +1,14 @@
+.onAttach <- function(libname, pkgname) {
+  packageStartupMessage(
+    paste0( "\nPlease cite the 'OPSR' package as:\n",
+            "Heimgartner, D, Xinyi, W., Mokhtarian P. L. and K. W. Axhausen (2024) ",
+            "OPSR: A package for estimating ordinal probit switching regression models in R. tbc.\n\n",
+            "Wang, X. and P. L. Mokhtarian (2024) ",
+            "Examining the treatment effect of teleworking on vehicle-miles driven: Applying an ordered probit selection model and incorporating the role of travel stress, ",
+            "Transportation Research Part A, 186, 104072, ",
+            "doi:10.1016/j.tra.2024.104072.\n\n",
+            "If you have questions, suggestions, or comments regarding the 'OPSR' ",
+            "package, please open an issue on https://github.com/dheimgartner/OPSR\n\n",
+            "To see these entries in BibTeX format, use 'citation('OPSR')'"),
+    domain = NULL,  appendLF = TRUE)
+}
