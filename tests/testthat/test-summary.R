245ee2c693bdc35b7f0dfadec7ad2ef4e5492ec8
dheimgartner <d.heimgartners@gmail.com>
2024-10-20 21:31:21 +0200

    added expected_output to minimally test print.summary.opsr

--- tests/testthat/test-summary.R
+++ tests/testthat/test-summary.R
@@ -2,7 +2,8 @@ test_that("runs without error for basic model", {
   sim_dat <- load_sim_dat()
   dat <- sim_dat$data
   fit <- opsr(ys | yo ~ xs1 + xs2 | xo1 + xo2, dat, printLevel = 0)
-  expect_no_error(summary(fit))
+  expect_no_error(s <- summary(fit))
+  expect_output(print(s))
 })
 
 test_that("runs without error for updated model", {
@@ -10,7 +11,8 @@ test_that("runs without error for updated model", {
   dat <- sim_dat$data
   fit <- opsr(ys | yo ~ xs1 + xs2 | xo1 + xo2, dat, printLevel = 0)
   fit_updated <- update(fit, ~ . | 1)
-  expect_no_error(summary(fit_updated))
+  expect_no_error(s <- summary(fit_updated))
+  expect_output(print(s))
 })
 
 test_that("runs without error for null model", {
@@ -18,6 +20,7 @@ test_that("runs without error for null model", {
   dat <- sim_dat$data
   fit <- opsr(ys | yo ~ xs1 + xs2 | xo1 + xo2, dat, printLevel = 0)
   fit_null <- opsr_null_model(fit, printLevel = 0)
-  expect_no_error(summary(fit_null))
+  expect_no_error(s <- summary(fit_null))
+  expect_output(print(s))
 })
 
