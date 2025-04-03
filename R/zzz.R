bcec08d797a167b8080f505533dd24b88050c75d
daniehei <daniel.heimgartner@ivt.baug.ethz.ch>
2025-04-03 12:09:42 +0200

    utils::

--- R/zzz.R
+++ R/zzz.R
@@ -1,5 +1,5 @@
 .onAttach <- function(libname, pkgname) {
-  please_cite <- capture.output(print(citation(pkgname)))
+  please_cite <- utils::capture.output(print(citation(pkgname)))
   please_cite <- paste(please_cite, collapse = "\n")
   packageStartupMessage(
     please_cite,
